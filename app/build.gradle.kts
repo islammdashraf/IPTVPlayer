ebaa63905157ab3e1188dfa10f225c61092479cd
MD ASHRAFUL ISLAM <86507368+islammdashraf@users.noreply.github.com>
2026-06-23 11:40:10 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
